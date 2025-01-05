652ad4c6ad33c25aaefc4a5ee7546e9df7e1e24b
V1kyCh <126974500+V1kyCh@users.noreply.github.com>
2025-01-05 14:15:46 +0200

    all done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // 1
 let time1 = 0
 const interval1 = setInterval(()=> {
-    alert('вивела повідомлення кожну секунду')
+    console.log('Вивожу повідомлення в консоль поки не пройде 5 секунд')
     time1++
     if (time1 === 5) {
         clearInterval(interval1)
@@ -10,37 +10,69 @@ const interval1 = setInterval(()=> {
 
 // 2
 
-const box1El = document.querySelector('.box1') 
-const box2El = document.querySelector('.box2') 
+const box1El = document.querySelector('.box1')
+const box2El = document.querySelector('.box2')
 
-let  marginBox = 10
+let marginBox = 10
 setInterval(() => {
-    box1El.style.marginLeft = marginBox ++ + 'px'
+    box1El.style.marginLeft = marginBox++ + 'px'
 
     const red = Math.floor(Math.random() * 256)
     const green = Math.floor(Math.random() * 256)
     const blue = Math.floor(Math.random() * 256)
 
     box2El.style.backgroundColor = `rgb(${red},${green},${blue})`
-},500)
+}, 500)
 
 // 3
 
-// const gameBtnEl = document.querySelector('.game-btn')
-// const counterTextEl = document.querySelector('.counter')
+const gameBtnEl = document.querySelector('.game-btn')
+const counterTextEl = document.querySelector('.counter')
+const whatBoxText = document.querySelector('.what-box-text')
+const largeContainer = document.querySelector('.large-box')
 
-// let counter = 0
+let time = 0
+let counter = 0
 
-// gameBtnEl.addEventListener('click', () => {
-//     setInterval(() => {
-//         console.log(counter)
-        
-//     },1000)
+gameBtnEl.addEventListener('click', (e) => {
+    gameBtnEl.disabled = true
 
-//     counter++
-//     counterTextEl.textContent = counter
-//     console.log(counter)
-// })
+    largeContainer.firstElementChild.classList.add('animation-first-mid-box')
+    largeContainer.lastElementChild.classList.add('animation-small-box')
+    largeContainer.firstElementChild.nextElementSibling.classList.add('animation-sec-mid-box')
+
+    let statusGame = 'started'
+    counter = 0
+    counterTextEl.textContent = `Результат: ${counter}`
+
+
+    let currentBox = largeContainer.children[Math.floor(Math.random() * largeContainer.children.length)]
+    whatBoxText.textContent = currentBox.id
+
+    largeContainer.addEventListener('click', (e) => {
+        if (e.target.id === currentBox.id && statusGame === 'started') {
+            counter++
+            counterTextEl.textContent = `Результат: ${counter}`
+
+            currentBox = largeContainer.children[Math.floor(Math.random() * largeContainer.children.length)]
+            whatBoxText.textContent = currentBox.id
+        }
+    })
+
+    const interval = setInterval(() => {
+        time++
+        if (time === 10) {
+            largeContainer.firstElementChild.classList.remove('animation-first-mid-box')
+            largeContainer.lastElementChild.classList.remove('animation-small-box')
+            largeContainer.firstElementChild.nextElementSibling.classList.remove('animation-sec-mid-box')
+            clearInterval(interval)
+            gameBtnEl.disabled = false
+            time = 0
+            statusGame = 'stop'
+            whatBoxText.textContent = ''
+        }
+    }, 1000)
+})
 
 // 4
 
@@ -49,7 +81,7 @@ const inputTime = document.querySelector('.input-time')
 inputTime.addEventListener('change', () => {
     const time4 = inputTime.value
     setTimeout(() => {
-        alert(`це повідомлення прийшло через ${time4} секунд`)
-    },time4)
+        alert(`це повідомлення прийшло через ${time4} мілісекунд`)
+    }, time4)
     inputTime.value = ''
 })
